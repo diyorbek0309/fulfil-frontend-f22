9f49a673a756c87bc2bf4e1259ede6eed77be359
Diyorbek <diyorbekolimov2000@gmail.com>
2022-08-22 22:54:28 +0500

    lesson 27

---
+++ 27-dars/main.js
@@ -0,0 +1,59 @@
+// alert("Hello World");
+
+// console.log("Hello World 123");
+
+// O'zgaruvchilar - Variables
+// Identifiers - O'zgaruvchi nomi (identifikator)
+
+// Data Types
+
+// string firstName = "Diyorbek Olimov";
+let firstName2 = "Diyorbek Olimov";
+let firstName3 = `Diyorbek Olimov`; // string
+var age = 22,
+  height = 185,
+  weight = 65; // number
+let myExperience = 2.5; // number
+let isMarried = false; // boolean (true - rost, false - yo'lg'on)
+let notGiven; // undefined
+// let Age = 23;
+// let AGE = 23;
+
+// let - keyword
+// ism - identifier
+// = - assignment operator
+// "Diyorbek" - value
+
+age = 23;
+
+// console.log(typeof isMarried);
+// console.log(typeof firstName2, typeof age, typeof myExperience);
+// console.log(age);
+
+// Operators
+let a = "24",
+  b = 24,
+  c = 9;
+// Arithmetic Operators
+
+// console.log(a + b); // 30
+// console.log(a - b); // 18
+// console.log(a * b); // 144
+// console.log(a / b); // 4
+// console.log(a % c); // 6, 24 : 9 = 2 (6q); % - modulo operator
+// console.log(c ** b); // ** - exponent operator
+// console.log("Oldin ", a++); // postincrement operator
+// console.log("Keyin ", a);
+// console.log(++b); // preincrement operator
+// console.log(c--); // postdecrement operator
+// console.log(--c); // predecrement operator
+
+// Comparison Operators
+
+// console.log(a > b); // true
+// console.log(a < b); // false
+// console.log(a <= b); // false
+console.log(a == b); // a va b tengmi? false
+// console.log(a != b); // a va b teng emasmi? true
+console.log(a === b); // a va b turi bir xilmi va qiymati tengmi?
+
