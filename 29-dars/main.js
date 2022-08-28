89289719d73f06bafe425162f57c0397ea1f2ea8
Diyorbek <diyorbekolimov2000@gmail.com>
2022-08-28 22:58:44 +0500

    lesson 29

---
+++ 29-dars/main.js
@@ -0,0 +1,82 @@
+// let age = 12;
+// let result = age >= 16 ? "Siz pasport olgansiz!" : "Siz pasport olmagansiz!";
+
+// console.log(result);
+
+// if (age >= 16) {
+//   console.log("Siz pasport olgansiz!");
+// } else {
+//   console.log("Siz pasport olmagansiz!");
+// }
+
+// console.log(age >= 16 ? "Siz pasport olgansiz!" : "Siz pasport olmagansiz!");
+
+// let experience = 4;
+// let result =
+//   experience <= 1
+//     ? "Siz juniorsiz!"
+//     : experience > 1 && experience < 3
+//     ? "Siz middlesiz!"
+//     : "Siz seniorsiz!";
+
+// console.log(result);
+
+// if (experience <= 1) {
+//   console.log("Siz juniorsiz!");
+// } else if (experience > 1 && experience < 3) { // true && false = false
+//   console.log("Siz middlesiz!");
+// } else {
+//   console.log("Siz seniorsiz!");
+// }
+
+// let numberOfDay = 1;
+
+// switch (numberOfDay) {
+//   case 1:
+//     console.log("Dushanba");
+//     break;
+//   case 2:
+//     console.log("Seshanba");
+//     break;
+//   case 3:
+//     console.log("Chorshanba");
+//     break;
+//   case 4:
+//     console.log("Payshanba");
+//     break;
+//   case 5:
+//     console.log("Juma");
+//     break;
+//   case 6:
+//     console.log("Shanba");
+//     break;
+//   case 7:
+//     console.log("Yakshanba");
+//     break;
+//   default:
+//     console.log("Bunday hafta kuni yo'q!");
+// }
+
+// if (numberOfDay === 1) {
+//   console.log("Dushanba");
+// } else if (numberOfDay === 2) {
+//   console.log("Seshanba");
+// } else if (numberOfDay === 3) {
+//   console.log("Chorshanba");
+// } else if (numberOfDay === 4) {
+//   console.log("Payshanba");
+// } else if (numberOfDay === 5) {
+//   console.log("Juma");
+// } else if (numberOfDay === 6) {
+//   console.log("Shanba");
+// } else if (numberOfDay === 7) {
+//   console.log("Yakshanba");
+// } else {
+//   console.log("Bunaqa hafta kuni topilmadi!");
+// }
+let firstName = "Diyorbek",
+  lastName = "Olimov";
+// let result = firstName + lastName;
+let result = `${firstName}----${lastName}`;
+
+console.log(result);
