7ae67b0e104812e2d2ff6266b6eb35d63b331008
Diyorbek <diyorbekolimov2000@gmail.com>
2022-08-31 22:36:25 +0500

    lesson 30

--- 29-dars/main.js
+++ 29-dars/main.js
@@ -74,9 +74,90 @@
 // } else {
 //   console.log("Bunaqa hafta kuni topilmadi!");
 // }
-let firstName = "Diyorbek",
-  lastName = "Olimov";
-// let result = firstName + lastName;
-let result = `${firstName}----${lastName}`;
+// let firstName = "Diyorbek",
+//   lastName = "Olimov";
+// // let result = firstName + lastName;
+// let result = `${firstName}----${lastName}`;
 
-console.log(result);
+// console.log(result);
+
+// Qodirjon
+
+// function juftToq(x) {
+//   x % 2 === 0 ? console.log("Juft") : console.log("Toq");
+// }
+
+// juftToq(4);
+
+// let age = 26;
+
+// function person() {
+//   if (age <= 16) {
+//     console.log("O`quvchi");
+//   } else if (age > 16 && age < 25) {
+//     console.log("Talaba");
+//   } else if (25 <= age && age < 60) {
+//     console.log("Ishchi");
+//   } else {
+//     console.log("Nafaqaxo`r");
+//   }
+// }
+
+// person();
+
+// let a = 10;
+// let b = 150;
+// let c = 20;
+
+// function MaxSum() {
+//   if (a === Math.max(a, b, c)) {
+//     if (b > c) {
+//       console.log(a, b, a + b);
+//     } else {
+//       console.log(a, c, a + c);
+//     }
+//   } else if (b === Math.max(a, b, c)) {
+//     if (a > c) {
+//       console.log(b, a, a + b);
+//     } else {
+//       console.log(b, c, c + b);
+//     }
+//   } else if (c === Math.max(a, b, c)) {
+//     if (a > b) {
+//       console.log(c, a, c + a);
+//     } else {
+//       console.log(c, b, c + b);
+//     }
+//   }
+// }
+
+// MaxSum();
+
+// function KabisaYearAndDays(year, month) {
+//   switch (month) {
+//     case "Yanvar":
+//     case "Mart":
+//     case "May":
+//     case "Iyul":
+//     case "Avgust":
+//     case "Oktyabr":
+//     case "Dekabr":
+//       console.log(month, " oyida 31 kun bor");
+//       break;
+//     case "Fevral":
+//       if (year % 4 === 0) console.log(month, " oyida 29 kun bor");
+//       else console.log(month, " oyida 28 kun bor");
+//       break;
+//     case "Aprel":
+//     case "Iyun":
+//     case "Sentyabr":
+//     case "Noyabr":
+//       console.log(month, " oyida 30 kun bor");
+// default: 
+// console.log("Bunday oy yo'q!")
+//   }
+// }
+
+// KabisaYearAndDays(1994, "Mart");
+
+
---
+++ 30-dars/main.js
@@ -0,0 +1,72 @@
+// let i = 0;
+
+// for (; i < 100000; i++) {
+//   console.log(i);
+// }
+
+// const person = { fname: "Diyorbek", lname: " Olimov" };
+
+// let text = "";
+
+// for (let x in person) {
+//   //1-qadam: x = fname
+//   //2-qadam: x = lname
+//   text = text + person[x];
+// }
+
+// console.log(text);
+
+// const cars = ["BMW", "Volvo", "Mini"];
+
+// let text = "";
+// for (let car of cars) {
+//   // 1-qadam: BMW
+//   // 2-qadam: Volvo
+//   // 3-qadam: Mini
+
+//   text = text + car;
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   text = text + cars[i];
+
+//   //   1-qadam: i = 0; text = ""
+//   //   2-qadam: i = 1; text = "BMW"
+//   //   3-qadam: i = 2; text = "BMWVolvo"
+//   //   4-qadam: i = 3; text = "BMWVolvoMini"
+// }
+
+// console.log(text);
+
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 100;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+// for (let i = 10; ; i = i + 5) {
+//   if (i % 1225 === 0) {
+//     console.log("Evrika: 1225!");
+//     break;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// 1 dan 10 gacha sonlar orasidan faqat toqlarini consolega chiqaring.
+
+// for (let i = 1; i < 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log("Juft son");
+//     continue;
+//     console.log("Continuedan pastgi console.log");
+//   } else {
+//     console.log("Toq son: ", i);
+//   }
+// }
