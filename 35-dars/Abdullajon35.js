2996e7c20f0e45236402b773bb31aa87285f6fdc
Diyorbek <diyorbekolimov2000@gmail.com>
2022-09-21 21:01:11 +0500

    lesson 36

---
+++ 35-dars/Abdullajon35.js
@@ -0,0 +1,14 @@
+// function dividesEvenly(a, b) {
+//   return a % b === 0;
+// }
+
+const dividesEvenly = (a, b) => a % b === 0;
+
+function divisibleByFive(n) {
+  if (n % 5 === 0) return true;
+  return false;
+}
+
+function equation(s) {
+  return eval(s);
+}
---
+++ 35-dars/Adham.js
@@ -0,0 +1,32 @@
+// 1
+// Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
+
+var a = "10";
+var b = "3";
+
+if (a % b === 0) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+// 2
+// ====
+
+// var a = ("-55");
+
+// if ( a % 5 === 0) {
+//     console.log("true")
+// }
+// else {
+//     console.log("false")
+// }
+
+// 3
+//     var a = ("5")
+//     var b = ("7")
+//     var c = ("3")
+// function equation() {
+//      eval("var d = a + b * c");
+//     console.log("d");
+// }
