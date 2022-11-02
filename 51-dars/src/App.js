c21ea92e3ee22230caa00db6a0617ecbbfe8e0ec
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2022-11-02 22:43:55 +0500

    lesson 51

---
+++ 51-dars/src/App.js
@@ -0,0 +1,32 @@
+// import { Component } from "react";
+import "./App.css";
+// import Button from "./Button";
+import { Button, Input } from "./Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Input />
+      <Button />
+      {/* <button>Button</button> */}
+    </div>
+  );
+}
+
+// class App extends Component {
+//   constructor() {}
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello World!</h1>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
+
+// Component - funksiya
+// 2 turi bor: class based va functional
---
+++ 51-dars/src/Button.jsx
@@ -0,0 +1,13 @@
+const Button = () => {
+  return <button>Button</button>;
+};
+
+// export default Button;
+
+const Input = () => {
+  return <input type="text" />;
+};
+
+export { Button, Input };
+
+// Exportlarni turlari: default, named (nomli)
